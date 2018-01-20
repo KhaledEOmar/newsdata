194edd1daf670b83347df00d5c01325e1f6d2026
Khaled Omar <khaledeomar@gmail.com>
2018-01-20 03:05:37 -0500

    Reporting Tool Functional and Readme

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# newsdata
+# NewsData
+NewsData is a log analysis reporting tool built in python. 
+
+# Installation
+In order to run NewsData: 
+
+1. Install Python 2.7.14 package, and ensure that the Python Path option is selected during installation.
+
+2. Install, Git Bash, Vagrant, and VirtualBox.
+
+3. Clone VM configuration using: 
+```
+git clone https://github.com/udacity/fullstack-nanodegree-vm
+```
+
+4. Clone NewsData into Vagrant directory:
+```
+git clone https://github.com/KhaledEOmar/newsdata.git
+```
+
+5. Open two Git Bash windows
+
+# Starting the Reporting Tool
+1. In first Bash window, run psql -d news
+2. In second Bash window, run Python loganalysis.py
--- loganalysis.py
+++ loganalysis.py
@@ -1,25 +1,53 @@
 import psycopg2
 
-def main(): 
+
+def main():
         db = psycopg2.connect("dbname=news")
 
         cursor = db.cursor()
 
-        print "Connected!\n"
+        print "Connected!\n\n"
 
-        cursor.execute("select path as path, count(*) as count FROM log WHERE path LIKE '%article%' GROUP BY path ORDER BY count(*) DESC LIMIT 3;")
-        #/article/ then slug
-        topthree = cursor.fetchall()
-        print topthree
+        cursor.execute("""SELECT Articles.Title, x.count from
+                        (SELECT substring(path from 10) as path, count(*)
+                        as count FROM log WHERE path LIKE '%article%' AND
+                        status = '200 OK' GROUP BY path ORDER BY count(*)
+                        DESC LIMIT 3) as x
+                        LEFT JOIN Articles ON x.path = Articles.Slug
+                        ORDER BY count DESC;""")
+        topThree = cursor.fetchall()
+        print "\n\n                    Top 3 Articles                   "
+        print "-----------------------------------------------------"
+        for article in topThree:
+                print article[0], " - ", article[1], " views"
 
-        cursor.execute("select path, count(*) as count FROM log WHERE path LIKE '%article%' GROUP BY path ORDER BY count(*) DESC LIMIT 3;")
-        topauthors = cursor.fetchall()
-        print topauthors
+        cursor.execute("""Select z.name as name, SUM(z.viewCount) from
+                        (SELECT Authors.Name, y.viewCount from
+                        (SELECT x.shortPath, Articles.Author, Articles.Title
+                        as articleTitle, x.viewCount from
+                        (SELECT substring(path from 10) as shortPath, count(*)
+                        as viewCount FROM log WHERE path LIKE '%article%' AND
+                        status = '200 OK' GROUP BY shortPath) as x
+                        LEFT JOIN Articles ON x.shortPath = Articles.Slug) as y
+                        INNER JOIN Authors ON y.author = Authors.id) as z
+                        GROUP BY name ORDER BY sum DESC;""")
+        topAuthors = cursor.fetchall()
+        print "\n\n                      Top  Authors                   "
+        print "-----------------------------------------------------"
+        for author in topAuthors:
+                print author[0], " - ", author[1], " views"
 
-        cursor.execute("select path as path, count(*) as count FROM log WHERE path LIKE '%article%' GROUP BY path ORDER BY count(*) DESC LIMIT 3;")
-        topthree = cursor.fetchall()
-        print topthree
+        cursor.execute("""Select timez, Percent from (SELECT timez, (100.00 *
+        Errors/Total) as Percent from (SELECT date_trunc('day',time) as timez,
+        count(status) filter (WHERE status LIKE '4%' OR status LIKE '5%') as
+        Errors, count(status) as Total from log GROUP BY timez ORDER BY timez)
+        as Totals) as Percentage WHERE Percent >= 1.00;""")
+        highErrors = cursor.fetchall()
+        print "\n\n                   High Error Days                   "
+        print "-----------------------------------------------------"
+        for errors in highErrors:
+                print errors[0].strftime('%b %d, %Y'), " - ",\
+                      "{0:.2f}".format(errors[1]), "% errors"
 
         db.close()
-
 main()
